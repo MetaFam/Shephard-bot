b3cd56e8ff5e655c3e510655199d580ec122d609
Vyvy-vi <vyom.j@protonmail.com>
2021-07-04 14:26:20 +0530

    chore: set up basic print logging

--- src/bot.py
+++ src/bot.py
@@ -14,19 +14,23 @@ class Bot(commands.Bot):
         super().__init__(
             command_prefix=consts.PREFIX, case_insensitive=True, intents=intents
         )
-        self.load_cogs()
 
     def load_cogs(self):
         """Load all the cogs for the bot"""
         for cog in consts.COGS:
             self.load_extension(cog)
+            print(f"Loaded cog: {cog}")
 
     def run(self):
         """Run the Bot"""
+        self.load_cogs()
         if (consts.TOKEN is None) or (consts.TOKEN == ""):
             raise EnvironmentError(
                 "Empty Token or No Token provided in the .env config"
             )
         if (consts.MONGO_URI is None) or (consts.MONGO_URI == ""):
             raise EnvironmentError("No Mongo URI found. Can't connect to database.")
+        if (consts.TOKEN == "foo"):
+            print("No token loaded, cog-loading completed")
+            return
         super().run(consts.TOKEN)
